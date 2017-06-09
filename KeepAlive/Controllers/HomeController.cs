b4aab93c7b5466a86c3111d696a7fb416ccb9881
P.J. Tenn <ptenn@arbfile.org>
2017-06-09 08:49:52 -0400

    Initial Commit

---
+++ KeepAlive/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeepAlive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static ILog logger = LogManager.GetLogger(typeof(HomeController));
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public void KeepAlive()
+        {
+            logger.Info("Execute KeepAlive Method.");
+        }
+    }
+}
